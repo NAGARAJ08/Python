8382976015e8dcff9fdad17447efca555c50bffd
unknown <kbnagaraj18@gmail.com>
2023-03-08 00:05:23 +0530

    added Basic Methods

--- App/main.py
+++ App/main.py
@@ -1,36 +1,79 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def msg():
     return "Welcome "
 
+
 @app.route('/square/<int:n>')
-def sum(n):
-    ans = n**2
-    
+def square(n):
+    ans = n ** 2
+
     res = {
-        "Number":n,
-        "Squared Value":ans
+        "Number": n,
+        "Squared Value": ans
     }
     return jsonify(res)
 
 
+@app.route('/add/<a>/<b>')
+def add(a, b):
+    ans = int(a) + int(b)
+    res = {
+        "Number 1": a,
+        "Number 2": b,
+        "Summation of Two Numbers : ": ans
+    }
+    return jsonify(res)
+
+
+@app.route('/sub/<a>/<b>')
+def sub(a, b):
+    ans = int(a) - int(b)
+    res = {
+        "Number 1": a,
+        "Number 2": b,
+        "Difference of Two Numbers : ": ans
+    }
+    return jsonify(res)
+
+
+@app.route('/mul/<a>/<b>')
+def mul(a, b):
+    ans = int(a) * int(b)
+    res = {
+        "Number 1": a,
+        "Number 2": b,
+        "Product of Two Numbers : ": ans
+    }
+    return jsonify(res)
+
+
+@app.route('/Div/<a>/<b>')
+def div(a, b):
+    ans = int(a) / int(b)
+    res = {
+        "Number 1": a,
+        "Number 2": b,
+        "Division of Two Numbers : ": ans
+    }
+    return jsonify(res)
+
 
 @app.route('/even-or-odd/<int:n>')
 def even_or_odd(n):
-    
-    if n%2==0:
-        ans =  "The Number is Even"
+    if n % 2 == 0:
+        ans = "The Number is Even"
     else:
-        ans =  "The Number is ODD"
-    
+        ans = "The Number is ODD"
+
     res = {
-        
-        "Number":n,
-        "Verdict":ans
+
+        "Number": n,
+        "Verdict": ans
     }
     return jsonify(res)
 
